1d1a3ff75346305b5fcbf0040d29d44c7be994fb
amine0089 <mohamedamine371@gmail.com>
2022-05-23 14:55:24 +0200

    yarab

--- projec/settings.py
+++ projec/settings.py
@@ -102,7 +102,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'build')
+            os.path.join(BASE_DIR, 'frontend/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
