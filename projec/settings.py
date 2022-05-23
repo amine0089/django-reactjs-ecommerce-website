e1a541d2be31a519f2596e9a704ddf8e3675c61c
amine0089 <mohamedamine371@gmail.com>
2022-05-23 16:58:45 +0200

    hayas

--- projec/settings.py
+++ projec/settings.py
@@ -102,7 +102,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'build')
+            os.path.join(BASE_DIR, 'frontend/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -169,7 +169,7 @@ MEDIA_URL = 'img/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "build/static"
+    BASE_DIR / "frontend/build/static"
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = 'static/img'
